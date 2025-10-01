bd36b4e952f9301a77d10d7b2659340623d4a4df
Camille Le Pennec <camille.lpennec@gmail.com>
2025-10-01 22:18:27 +0200

    user login signinData

--- src/signup.rs
+++ src/signup.rs
@@ -3,6 +3,70 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SigninData {
+    username: String,
+    password: String,
+}
+impl SigninData {
+    pub fn new(username: String, password: String) -> Self {
+        Self { username, password }
+    }
+    pub fn username(&self) -> &str {
+        self.username.as_str()
+    }
+    pub fn password(&self) -> &str {
+        self.password.as_str()
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SigninResponseData {
+    username: String,
+    unique_id: Uuid,
+    user_creation_ts: DateTime<Utc>,
+    jwt: String,
+    first_experiment_display_context_id: Uuid,
+    experiment_display_context_creation_ts: DateTime<Utc>,
+}
+
+impl SigninResponseData {
+    pub fn new(
+        username: &str,
+        uuid: Uuid,
+        user_creation_ts: DateTime<Utc>,
+        jwt: String,
+        first_experiment_display_context_id: Uuid,
+        experiment_display_context_creation_ts: DateTime<Utc>,
+    ) -> Self {
+        Self {
+            username: username.to_owned(),
+            unique_id: uuid,
+            user_creation_ts,
+            jwt,
+            first_experiment_display_context_id,
+            experiment_display_context_creation_ts,
+        }
+    }
+    pub fn id(&self) -> Uuid {
+        self.unique_id
+    }
+    pub fn username(&self) -> &str {
+        self.username.as_str()
+    }
+    pub fn jwt(&self) -> &str {
+        self.jwt.as_str()
+    }
+    pub fn first_experiment_display_context_id(&self) -> Uuid {
+        self.first_experiment_display_context_id
+    }
+    pub fn user_creation_ts(&self) -> DateTime<Utc> {
+        self.user_creation_ts
+    }
+    pub fn experiment_display_context_creation_ts(&self) -> DateTime<Utc> {
+        self.experiment_display_context_creation_ts
+    }
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SignupData {
     username: String,
     password: String,
