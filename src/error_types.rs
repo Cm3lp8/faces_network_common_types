80b957fa80d6f5ebba11b29268e79eb97db9eac7
Camille Le Pennec <camille.lpennec@gmail.com>
2025-10-01 23:25:26 +0200

    user login signinData5

--- src/error_types.rs
+++ src/error_types.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum UserLoginResponseErrorKind {
     WrongPwd { user_name: String },
     UserNotFound { user_name: String },
