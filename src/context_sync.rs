9a7dba2e368e0e58858def360f227bbd1aa506b5
Camille Le Pennec <camille.lpennec@gmail.com>
2025-10-18 13:05:41 +0200

    when context changing, query last context last known context version4

--- src/context_sync.rs
+++ src/context_sync.rs
@@ -26,13 +26,19 @@ impl LastClientContextVersion {
 #[derive(Debug, Clone, Serialize, Deserialize, Decode, Encode)]
 pub struct PulledContextVersionWithAnimationDelta {
     context_id: [u8; 16], // UUid
-    animation_delta: Vec<[u8; 16]>,
+    animation_delta: AnimationDeltaCollection,
 }
 impl PulledContextVersionWithAnimationDelta {
-    pub fn new(context_id: Uuid, animations_delta: &[Uuid]) -> Self {
+    pub fn new(context_id: Uuid, animations_delta: Option<&[Uuid]>) -> Self {
+        let animation_delta: AnimationDeltaCollection = match animations_delta {
+            Some(coll) => AnimationDeltaCollection::AnimationDelta {
+                ids_collection: coll.iter().map(|it| it.into_bytes()).collect(),
+            },
+            None => AnimationDeltaCollection::UptoDate,
+        };
         Self {
             context_id: context_id.into_bytes(),
-            animation_delta: animations_delta.iter().map(|it| it.into_bytes()).collect(),
+            animation_delta,
         }
     }
 
@@ -40,10 +46,22 @@ impl PulledContextVersionWithAnimationDelta {
         Uuid::from_bytes(self.context_id)
     }
 
-    pub fn get_animation_delta_ids_coll(&self) -> Vec<Uuid> {
-        self.animation_delta
-            .iter()
-            .map(|i| Uuid::from_bytes(*i))
-            .collect()
+    pub fn get_animation_delta_ids_coll(&self) -> Option<Vec<Uuid>> {
+        let AnimationDeltaCollection::AnimationDelta { ids_collection } = &self.animation_delta
+        else {
+            return None;
+        };
+        Some(
+            ids_collection
+                .iter()
+                .map(|i| Uuid::from_bytes(*i))
+                .collect(),
+        )
     }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Decode, Encode)]
+pub enum AnimationDeltaCollection {
+    UptoDate,
+    AnimationDelta { ids_collection: Vec<[u8; 16]> },
 }
