275ea9336de8e303c9087c1fc5a6777138f72a6b
Camille Le Pennec <camille.lpennec@gmail.com>
2025-11-30 10:47:43 +0100

    server context version tracks 2 values : context version and user session version

--- src/context_version.rs
+++ src/context_version.rs
@@ -2,22 +2,30 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Hash, PartialEq, Clone, Eq)]
+/// [`ServerContextVersion`] represents two version counter states from the server.
+/// It shows the actual server session version for the user and a specific context version
 pub struct ServerContextVersion {
     context_id: Uuid,
-    version: u64,
+    context_version: u64,
+    user_session_version: u64,
 }
 
 impl ServerContextVersion {
-    pub fn new(context_id: Uuid, version: u64) -> Self {
+    pub fn new(context_id: Uuid, context_version: u64, user_session_version: u64) -> Self {
         Self {
             context_id,
-            version,
+
+            context_version,
+            user_session_version,
         }
     }
     pub fn context_id(&self) -> Uuid {
         self.context_id
     }
     pub fn context_version(&self) -> u64 {
-        self.version
+        self.context_version
+    }
+    pub fn user_session_version(&self) -> u64 {
+        self.user_session_version
     }
 }
