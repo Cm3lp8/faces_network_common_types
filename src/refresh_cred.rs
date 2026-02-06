bb11e01063e2f504716f3c58052c371d94842e58
Camille Le Pennec <camille.lpennec@gmail.com>
2026-02-06 15:17:37 +0100

    fix for wrong list of participant for display context in db, on invitation response2

--- src/refresh_cred.rs
+++ src/refresh_cred.rs
@@ -14,3 +14,40 @@ impl RefreshToken {
         &self.token
     }
 }
+
+#[derive(Clone, Encode, Decode)]
+pub struct JwtToken {
+    token: String,
+}
+
+impl JwtToken {
+    pub fn new(token: String) -> Self {
+        Self { token }
+    }
+
+    pub fn token(&self) -> &str {
+        &self.token
+    }
+}
+
+#[derive(Clone, Encode, Decode)]
+pub struct NewGeneratedAuthTokens {
+    refresh_token: RefreshToken,
+    jwt_token: JwtToken,
+}
+
+impl NewGeneratedAuthTokens {
+    pub fn new(refresh_token: String, jwt_token: String) -> Self {
+        Self {
+            refresh_token: RefreshToken::new(refresh_token),
+            jwt_token: JwtToken::new(jwt_token),
+        }
+    }
+
+    pub fn refresh_token(&self) -> &str {
+        &self.refresh_token.token()
+    }
+    pub fn jwt(&self) -> &str {
+        &self.jwt_token.token()
+    }
+}
