a46d56878d34b1288c5e6c8429f845a687d75e53
Camille Le Pennec <camille.lpennec@gmail.com>
2026-03-02 11:01:18 +0100

    composition network shareable type

--- src/compositions.rs
+++ src/compositions.rs
@@ -2,32 +2,63 @@ use uuid::Uuid;
 
 use crate::context_ressources::RessourceItem;
 
-pub struct Composition {
+pub struct CompositionData {
     id: [u8; 16],
     author_id: [u8; 16],
-    /// Dimensions in pixel of the bounding box
-    size: [f32; 2],
-    normalized_pos: [f32; 2],
-    ///
-    resource_ids_set: Vec<[u8; 16]>,
+    width: u32,
+    height: u32,
+    min_x: f32,
+    max_x: f32,
+
+    min_y: f32,
+    max_y: f32,
 }
 
-impl Composition {
-    pub fn new(id: Uuid, author_id: Uuid) -> CompositionBuilder {
-        CompositionBuilder {
-            resources_set: vec![]
+impl CompositionData {
+    pub fn new(
+        id: Uuid,
+        author_id: Uuid,
+        width: u32,
+        height: u32,
+        min_x: f32,
+        max_x: f32,
+        min_y: f32,
+        max_y: f32,
+    ) -> Self {
+        Self {
+            id: id.into_bytes(),
+            author_id: author_id.into_bytes(),
+            width,
+            height,
+            min_x,
+            max_x,
+            min_y,
+            max_y,
         }
     }
+
     pub fn id(&self) -> Uuid {
         Uuid::from_bytes(self.id)
     }
-}
-
-pub struct CompositionBuilder {
-    resources_set: Vec<RessourceDimData>,
-}
-
-pub struct RessourceDimData {
-    size: [f32; 2],
-    screen_pos: [f32; 2],
+    pub fn author_id(&self) -> Uuid {
+        Uuid::from_bytes(self.author_id)
+    }
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+    pub fn min_x(&self) -> f32 {
+        self.min_x
+    }
+    pub fn max_x(&self) -> f32 {
+        self.max_x
+    }
+    pub fn min_y(&self) -> f32 {
+        self.min_y
+    }
+    pub fn max_y(&self) -> f32 {
+        self.max_y
+    }
 }
